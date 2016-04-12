8f00a008c0f2f0e9458696846c3cf01300058c9e
Chauncey McAskill <shimoroka@gmail.com>
2016-04-11 23:28:34 -0400

    WIP : Added 'Single Post Template' post type feature

--- src/PostType/Support/FeatureInterface.php
+++ src/PostType/Support/FeatureInterface.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Thelonius framework.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link      https://github.com/mcaskill/thelonius
+ * @copyright Copyright © 2016 Chauncey McAskill
+ * @license   https://github.com/mcaskill/thelonius/blob/master/LICENSE (MIT License)
+ */
+
 namespace Thelonius\PostType\Support;
 
 use Thelonius\Support\SupportableInterface;
@@ -7,6 +18,6 @@ use Thelonius\Support\SupportableInterface;
 /**
  * Post Type Feature Interface
  */
-interface FeatureInterface
+interface FeatureInterface extends SupportableInterface
 {
 }
--- src/Support/SupportableInterface.php
+++ src/Support/SupportableInterface.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Thelonius framework.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link      https://github.com/mcaskill/thelonius
+ * @copyright Copyright © 2016 Chauncey McAskill
+ * @license   https://github.com/mcaskill/thelonius/blob/master/LICENSE (MIT License)
+ */
+
 namespace Thelonius\Support;
 
 /**
