5556071c21ff35a8b0477e1af52ae17d89f00fae
Chauncey McAskill <chauncey@locomotive.ca>
2016-04-13 18:20:17 -0400

    WIP : Added DatabaseManager and PostType interface concepts.

---
+++ src/PostType/PostTypeInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Thelonius framework.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link      https://github.com/mcaskill/thelonius
+ * @copyright Copyright © 2016 Chauncey McAskill
+ * @license   https://github.com/mcaskill/thelonius/blob/master/LICENSE (MIT License)
+ */
+
+namespace Thelonius\PostType;
+
+/**
+ * Defines a WordPress post type model.
+ */
+interface PostTypeInterface
+{
+    /**
+     * Retrieve the post data as a {@see wp_insert_post()} compatible array.
+     *
+     * @return array
+     */
+    public function getPostData();
+}
--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,34 @@
 <?php
+
+use WP_Error;
+
+use Thelonius\PostType\PostTypeInterface;
+
+if ( ! function_exists('wp_insert_post_object') ) {
+    /**
+     * Insert or update a post.
+     *
+     * @uses wp_insert_post()
+     *
+     * @param PostTypeInterface  $post      A post object to update or insert.
+     * @param boolean            $wp_error  Optional. Whether to allow return of WP_Error on failure. Default false.
+     *
+     * @return integer|WP_Error  The post ID on success. The value 0 or WP_Error on failure.
+     *
+     * @todo Move this to a `PostManager::insert()` class / method.
+     */
+    function wp_insert_post_object(PostTypeInterface $post, $wp_error = false)
+    {
+        $post_id = wp_insert_post($post->getPostData(), $wp_error);
+
+        if ( 0 === $post_id || $post_id instanceof WP_Error ) {
+            return $post_id;
+        }
+
+        foreach ( $post->getPostMeta() as $key => $value ) {
+            update_post_meta($post_id, $key, $value);
+        }
+
+        return $post_id;
+    }
+}
